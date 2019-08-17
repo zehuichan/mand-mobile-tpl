5dc19b8f9996a2f3e842d7ac68cb1b76e985f6da
jasconchan <jasonchenzehui@gmail.com>
2019-08-17 21:26:06 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,62 @@
+const path = require('path').browserslistrc
+  .editorconfig
+  .env.development
+  .env.production
+  .env.staging
+  .gitignore
+babel.config.js
+postcss.config.js
+vue.config.js
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = 'vue Mand Mobile Tpl' // page title
+
+const port = 9527 // dev port
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development' ? 'error' : false,
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: false,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    // proxy: process.env.VUE_APP_BASE_API,
+    proxy: {
+      // change xxx-api/login => mock/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://127.0.0.1:${port}/mock`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    },
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  chainWebpack(config) {
+    config
+    // https://webpack.js.org/configuration/devtool/#development
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map')
+      )
+  }
+}
